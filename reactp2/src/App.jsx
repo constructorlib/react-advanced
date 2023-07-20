18cf4686c3c4232791c98d960cdd89dfc8bf51fb
constructorlib <constructorlib@gmail.com>
2023-07-20 23:11:14 +0900

    async

--- reactp2/src/App.jsx
+++ reactp2/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Setup from "./tutorial/1-useState/setup/1-error-example";
 // import ArrState from "./tutorial/1-useState/setup/3-useState-array";
 // import ArrState from "./tutorial/1-useState/setup/4-useState-object";
-import ArrState from "./tutorial/2-useEffect/setup/2-useEffect-cleanup";
+import ArrState from "./tutorial/2-useEffect/setup/3-useEffect-fetch-data";
 function App() {
   return (
     <div className="container">
---
+++ reactp2/src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.jsx
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from "react";
+
+const url = "https://api.github.com/users";
+
+const UseEffectFetchData = () => {
+  const [users, setUsers] = useState([]);
+  const getUsers = async () => {
+    const response = await fetch;
+  };
+  useEffect(() => {});
+  return (
+    <>
+      <h3>github users</h3>
+    </>
+  );
+};
+
+export default UseEffectFetchData;
