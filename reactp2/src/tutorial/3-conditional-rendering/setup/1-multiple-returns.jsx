287c9ae1f1e374667bc1d907ef9bd3ac48a22ac1
constructorlib <constructorlib@gmail.com>
2023-07-23 17:09:01 +0900

    I forgot to apply for course counselling mate

--- reactp2/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.jsx
+++ reactp2/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.jsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from "react";
 const url = "https://api.github.com/users/QuincyLarson";
 const MultipleReturns = () => {
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [user, setUser] = useState("dflt usr");
   if (loading) return <h2>loading...</h2>;
+  if (isError) return <h2>error... </h2>;
+  return <p>Hey!</p>;
   // I can set up whatever here
-  else return <p>Hey!</p>;
 };
 
 export default MultipleReturns;
