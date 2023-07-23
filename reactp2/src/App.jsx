37d76e4ede8dd252224fc2d0624e5bb9640aeeb6
constructorlib <constructorlib@gmail.com>
2023-07-23 16:32:30 +0900

    new day, new commits

--- reactp2/src/App.jsx
+++ reactp2/src/App.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import Setup from "./tutorial/1-useState/setup/1-error-example";
 // import ArrState from "./tutorial/1-useState/setup/3-useState-array";
 // import ArrState from "./tutorial/1-useState/setup/4-useState-object";
+import CondRender from "./tutorial/3-conditional-rendering/setup/1-multiple-returns";
 import ArrState from "./tutorial/2-useEffect/setup/3-useEffect-fetch-data";
 function App() {
   return (
     <div className="container">
       {/* <h2>Advanced React Tutorial</h2> */}
       {/* <Setup /> */}
-      <ArrState />
+      {/* <ArrState /> */}
+      <CondRender />
     </div>
   );
 }
