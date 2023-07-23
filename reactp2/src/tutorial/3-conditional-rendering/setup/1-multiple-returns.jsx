8328e7e4037d773f99b67b9652f2deb9cbc87288
constructorlib <constructorlib@gmail.com>
2023-07-23 21:29:29 +0900

    cb

--- reactp2/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.jsx
+++ reactp2/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.jsx
@@ -1,13 +1,26 @@
 import React, { useState, useEffect } from "react";
 const url = "https://api.github.com/users/QuincyLarson";
-const MultipleReturns = () => {
-  const [loading, setLoading] = useState(true);
-  const [isError, setIsError] = useState(false);
-  const [user, setUser] = useState("dflt usr");
-  if (loading) return <h2>loading...</h2>;
-  if (isError) return <h2>error... </h2>;
-  return <p>Hey!</p>;
-  // I can set up whatever here
+
+const names = ["Abbas", "Fatima", "Aziz", "Sumiya"];
+
+const myForEach = (arr, cb) => {
+  for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+    cb(element);
+  }
 };
+myForEach(names, (name) => {
+  console.log(name);
+});
+// const MultipleReturns = () => {
+//   const [loading, setLoading] = useState(true);
+//   const [isError, setIsError] = useState(false);
+//   const [user, setUser] = useState("dflt usr");
+//   if (loading) return <h2>loading...</h2>;
+//   if (isError) return <h2>error... </h2>;
+//   return <p>Hey!</p>;
+
+//   // I can set up whatever here
+// };
 
 export default MultipleReturns;
