d1acea7da709472e374f1c37885280028077a299
constructorlib <constructorlib@gmail.com>
2023-07-23 16:47:33 +0900

    nice, I can put a whole app into a condition.

--- reactp2/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.jsx
+++ reactp2/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
 const url = "https://api.github.com/users/QuincyLarson";
 const MultipleReturns = () => {
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   if (loading) return <h2>loading...</h2>;
+  // I can set up whatever here
   else return <p>Hey!</p>;
 };
 
