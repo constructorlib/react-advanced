6ef94b07f8efe9f1c10af965221a07bf4ee06e4c
constructorlib <constructorlib@gmail.com>
2023-07-26 23:07:06 +0900

    bro

--- reactp2/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.jsx
+++ reactp2/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.jsx
@@ -1,29 +1,40 @@
 import React, { useState, useEffect } from "react";
 const url = "https://api.github.com/users/QuincyLarson";
 
-const names = ["Abbas", "Fatima", "Aziz", "Sumiya"];
+// const names = ["Abbas", "Fatima", "Aziz", "Sumiya"];
 
-const myForEach = (arr, cb) => {
-  for (let i = 0; i < arr.length; i++) {
-    const element = arr[i];
-    cb(element);
-  }
-};
+// const myForEach = (arr, cb) => {
+//   for (let i = 0; i < arr.length; i++) {
+//     const element = arr[i];
+//     cb(element);
+//   }
+// };
 
-myForEach(names, console.log);
-
+// myForEach(names, console.log);
+function orderPizza(callback) {
+  setTimeout(() => {
+    let pizza = "(V)";
+    callback(pizza); // ??
+  }, 2000);
+}
+function isReady(pizza) {
+  console.log(`Eat me ${pizza}`);
+}
+orderPizza(isReady);
+console.log("Call Abu Bakr");
+// Async = part of code depends on some other part of code
 // function myLog(name) {
 //   console.log(name);
 // }
-// const MultipleReturns = () => {
-//   const [loading, setLoading] = useState(true);
-//   const [isError, setIsError] = useState(false);
-//   const [user, setUser] = useState("dflt usr");
-//   if (loading) return <h2>loading...</h2>;
-//   if (isError) return <h2>error... </h2>;
-//   return <p>Hey!</p>;
+const MultipleReturns = () => {
+  const [loading, setLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [user, setUser] = useState("dflt usr");
+  if (loading) return <h2>loading...</h2>;
+  if (isError) return <h2>error... </h2>;
+  return <p>Hey!</p>;
 
-//   // I can set up whatever here
-// };
+  //   // I can set up whatever here
+};
 
 export default MultipleReturns;
