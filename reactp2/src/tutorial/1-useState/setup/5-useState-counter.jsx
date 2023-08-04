24a08627339453c27c0fb50ab457764faeda6dfb
constructorlib <constructorlib@gmail.com>
2023-08-04 22:37:52 +0500

    5-counter

--- reactp2/src/tutorial/1-useState/setup/5-useState-counter.jsx
+++ reactp2/src/tutorial/1-useState/setup/5-useState-counter.jsx
@@ -9,10 +9,17 @@ const UseStateCounter = () => {
       });
     }, 2000);
   };
+  const complexDecrease = () => {
+    setTimeout(() => {
+      setValue((prevState) => {
+        return prevState - 1;
+      });
+    }, 2000);
+  };
   return (
     <>
       <section style={{ margin: "4rem 0" }}>
-        <h2>Regular counterm </h2>
+        <h2>Regular counter </h2>
         <h1>{value}</h1>
         <button className="btn" onClick={() => setValue(value - 1)}>
           decrease
@@ -24,10 +31,13 @@ const UseStateCounter = () => {
           increase
         </button>
         <br /> <br />
-        <h2>Delayed Countdown</h2>
+        <h2>Delayed Counter</h2>
         <h1>{value}</h1>
         <button className="btn" onClick={complexIncrease}>
           increase later
+        </button>
+        <button className="btn" onClick={complexDecrease}>
+          decrease later
         </button>
       </section>
     </>
@@ -35,3 +45,14 @@ const UseStateCounter = () => {
 };
 
 export default UseStateCounter;
+
+/*
+value, setValue: default being 0
+complexIncrease()
+basically a delayed counter. Where does prevstate come from?
+ setValue((prevState) => {
+setValue being 0? Yeah. 
+Both delayed and normal counter work on the same value
+prevState being connected to current value, we get n + 1 always. 
+So this is pretty much everything I can do here, maybe make it more effecient later. 
+*/
