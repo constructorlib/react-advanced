61b499571daee4d9ca1927a3de41a4518a4299ca
constructorlib <constructorlib@gmail.com>
2023-08-07 12:42:58 +0500

    something happened

--- reactp2/src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.jsx
+++ reactp2/src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.jsx
@@ -1,60 +1,23 @@
-// import React, { useState, useEffect } from "react";
-
-// const url = "https://api.github.com/users";
-
-// const UseEffectFetchData = () => {
-//   const [users, setUsers] = useState([]);
-//   console.log("hey");
-//   const getUsers = async () => {
-//     const response = await fetch(url);
-//     const users = await response.json(url);
-//     // console.log(users);
-//     setUsers(users);
-//   };
-//   useEffect(() => {
-//     getUsers();
-//   });
-//   return (
-//     <>
-//       <h3>github users</h3>
-//       <ul className="users">
-//         {users.map((user) => {
-//           const { id, login, avatar_url, html_url } = user;
-//           return (
-//             <li key={id}>
-//               <img src={avatar_url} alt={login} />
-//               <div>
-//                 <h4>{login}</h4>
-//                 <a href={html_url}>profile</a>
-//               </div>
-//             </li>
-//           );
-//         })}
-//       </ul>
-//     </>
-//   );
+// const fetchData = () => {
+//   const axiosRequest = require("axios");
+//   axiosRequest
+//     .get("https://www.boredapi.com/api/activity")
+//     .then((response) => {
+//       console.log(`You could ${response.data.activity}`);
+//     })
+//     .catch((error) => {
+//       console.log(`Error ${error}`);
+//     });
 // };
-
-// export default UseEffectFetchData;
-
-// /*
-// Look at it tomorrow.
-// And start 3-conditional rendering from scratch.
-// */
-// const axiosRequest = require("axios");
-// let response = axiosRequest.get("https://www.boredapi.com/api/activity");
-// console.log(`You could: ${response.data.activity}`);
-// export default UseEffectFetchData;
-import React, { useState, useEffect } from "react";
-const axios = require("axios");
+// export default fetchData;
+import axios from "axios";
 
 const fetchData = async () => {
   try {
     const response = await axios.get("https://www.boredapi.com/api/activity");
-    console.log(`You could: ${response.data.activity}`);
-    // Use the response data here or return it as needed
+    console.log(`You could ${response.data.activity}`);
   } catch (error) {
-    console.error("Error fetching data:", error.message);
+    console.log(`Error ${error}`);
   }
 };
 
