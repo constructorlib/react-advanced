7de9e73dacef9d0caf9d6b58c9f44ac6acf3dbeb
constructorlib <constructorlib@gmail.com>
2023-11-06 15:24:23 +0900

    toggle error

--- reactp2/src/tutorial/3-conditional-rendering/setup/2-short-circuit.jsx
+++ reactp2/src/tutorial/3-conditional-rendering/setup/2-short-circuit.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 const ShortCircuit = () => {
   const [text, setText] = useState("");
+  const [isError, setError] = useState("false");
   const firstValue = text || "hello world";
   const secondValue = text && "hello world";
 
@@ -10,10 +11,15 @@ const ShortCircuit = () => {
       <div>
         {/* <h1>{firstValue}</h1>
         <h1>value: {secondValue}</h1> */}
-        {!text && <h1>Dinora</h1>}
+        {/* {!text && <h1>Dinora</h1>}
         {text && <h1>Dinora 1447</h1>}
         <button className="btn" onClick={() => setText("1447")}>
           click me!
+        </button> */}
+        {isError && <h1>No error!</h1>}
+        {!isError && <h1>Error...</h1>}
+        <button className="btn" onClick={() => setError(!isError)}>
+          Toggle error
         </button>
       </div>
     </>
