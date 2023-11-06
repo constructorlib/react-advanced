751ce60a07fe3f52683572bceefe90cf6c1920b1
constructorlib <constructorlib@gmail.com>
2023-11-06 13:48:07 +0900

    circuit js

--- reactp2/src/App.jsx
+++ reactp2/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Setup from "./tutorial/3-conditional-rendering/setup/1-multiple-returns";
+import Setup from "./tutorial/3-conditional-rendering/setup/2-short-circuit";
 // import ArrState from "./tutorial/1-useState/setup/3-useState-array";
 // import ArrState from "./tutorial/1-useState/setup/4-useState-object";
 // import CondRender from "./tutorial/3-conditional-rendering/setup/1-multiple-returns";
@@ -7,10 +7,6 @@ import ArrState from "./tutorial/2-useEffect/setup/3-useEffect-fetch-data";
 function App() {
   return (
     <div className="container">
-      {/* <h2>Advanced React Tutorial</h2> */}
-      {/* <Setup /> */}
-      {/* <ArrState /> */}
-      {/* <CondRender /> */}
       <Setup />
     </div>
   );
