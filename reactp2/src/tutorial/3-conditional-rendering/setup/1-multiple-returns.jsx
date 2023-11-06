e9c10134b142f3c4f178cd07812e70c70f2aa535
constructorlib <constructorlib@gmail.com>
2023-11-06 13:46:10 +0900

    fetch

--- reactp2/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.jsx
+++ reactp2/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.jsx
@@ -1,37 +1,6 @@
 import React, { useState, useEffect } from "react";
 const url = "https://api.github.com/users/QuincyLarson";
 
-// const names = ["Abbas", "Fatima", "Aziz", "Sumiya"];
-
-// const myForEach = (arr, cb) => {
-//   for (let i = 0; i < arr.length; i++) {
-//     const element = arr[i];
-//     cb(element);
-//   }
-// };
-
-// myForEach(names, console.log);
-
-// function orderPizza(callback) {
-//   setTimeout(() => {
-//     let pizza = "🍕";
-//     callback(pizza);
-//   }, 2000);
-// }
-
-// function isReady(pizza) {
-//   console.log(`Eat me ${pizza}`);
-// }
-
-// orderPizza(isReady);
-
-// // Removed the problematic if block.
-// console.log("Call Abu Bakr");
-
-// Async = part of code depends on some other part of code
-// function myLog(name) {
-//   console.log(name);
-// }
 const MultipleReturns = () => {
   const [loading, setLoading] = useState(true);
   const [isError, setIsError] = useState(false);
@@ -56,7 +25,7 @@ const MultipleReturns = () => {
     </div>
   );
 
-  //   // I can set up whatever here
+  //
 };
 
 export default MultipleReturns;
