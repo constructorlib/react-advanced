490f1a38dad675e96c115ba1c23576468aa83aed
constructorlib <constructorlib@gmail.com>
2023-11-06 14:33:28 +0900

    short-circuit evaluation

--- reactp2/src/tutorial/3-conditional-rendering/setup/2-short-circuit.jsx
+++ reactp2/src/tutorial/3-conditional-rendering/setup/2-short-circuit.jsx
@@ -1,12 +1,27 @@
 import React, { useState } from "react";
-// short-circuit evaluation
-// ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [text, setText] = useState("");
+  const firstValue = text || "hello world";
+  const secondValue = text && "hello world";
 
-  return <h2>short circuit</h2>;
+  return (
+    <>
+      <div>
+        {/* <h1>{firstValue}</h1>
+        <h1>value: {secondValue}</h1> */}
+        {!text && <h1>Dinora</h1>}
+        {text && <h1>Dinora 1447</h1>}
+        <button className="btn" onClick={() => setText("1447")}>
+          click me!
+        </button>
+      </div>
+    </>
+  );
 };
-
+/*  
+    Notice how it works different to plain discrete math:
+    ya'ni: if A, then B; 
+    whereas it usually works if A and B then ...
+*/
 export default ShortCircuit;
